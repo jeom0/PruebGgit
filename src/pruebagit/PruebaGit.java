934640f84db82c46be013e79fd6b26bc77cfb0d0
jeom0 <jeom.245@gmail.com>
2022-04-23 10:43:58 -0500

    Actualizacion prueba rama1

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -14,7 +14,7 @@ public class PruebaGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.err.println("Hola Mundo");// TODO code application logic here
     }
     
 }
